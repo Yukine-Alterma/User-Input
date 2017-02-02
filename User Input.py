f881ffcbc72949f625e345fb1c76c3f5bcd9116a
Yukine-Alterma <saphira1904@hotmail.com>
2017-02-02 03:47:07 -0500

    User Input
    Beginner Python Lesson #1
    User input and base calculations

---
+++ User Input.py
@@ -0,0 +1,15 @@
+name = input("Enter your name :")
+age = int(input("Enter your age : "))
+
+print("Your name is " +name+ ".")
+print("You are "+str(age)+ " years old.")
+
+solution = (100 - int(age))
+year = (2017 + 77)
+
+print("In the year "+str(year)+" you will be 100.")
+
+num = int(input("Choose a number : "))
+print(num * "Copy this\n")
+
+
